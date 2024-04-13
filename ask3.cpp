5fb8d6085f5f7407daf5e2795715e5bda62bb92b
Giorgos Dovas <georgez13@hotmail.gr>
2024-04-13 22:53:19 +0300

    Compiles now

--- ask3.cpp
+++ ask3.cpp
@@ -1,89 +1,80 @@
 #include "ask3.h"
 
-
-
-CoordinatesXY calculate_centerMass(Planet p1, Planet p2){
+CoordinatesXY calculate_centerMass(Planet p1, Planet p2)
+{
     long double mass = p1.getMass() + p2.getMass();
-    long double x = (p1.getX()*p1.getMass() + p2.getX()*p2.getMass()) / mass;
-    long double y = (p1.getY()*p1.getMass() + p2.getY()*p2.getMass()) / mass;
-    return CoordinatesXY(x,y);
+    long double x = (p1.getX() * p1.getMass() + p2.getX() * p2.getMass()) / mass;
+    long double y = (p1.getY() * p1.getMass() + p2.getY() * p2.getMass()) / mass;
+    return CoordinatesXY(x, y);
 }
 
-long double calculate_gravity(Planet p1, Planet p2){
-    long double gravity =1;
-
-    return gravity;
+long double calculate_gravity(long double m1, long double m2, long double distance)
+{
+    const long double G = 6.673e-11;
+    return (G * m1 * m2) / (distance * distance);
+}
+long double calculate_distance(CoordinatesXY xy1, CoordinatesXY xy2)
+{
+    return 1;
+}
+bool node_contains_planet(Planet planet, Square *root)
+{
+    int i;
+    // Could be a better way to do this with parents
+    for (auto &index : root->planetIndexes)
+    {
+        if (planet == root->planets->at(index))
+            return 1;
+    }
+    return 0;
 }
 
-void netForce(Planet &planet, Square &node){
-    if(node.planetIndexes.size()<1)
-        return; // fail condition
-    long double netforce =1;
-    Planet* squarePlanet;
-    // start from root
-    double dx,dy;
-
-    double dx = planet.getX() - node.xy.x;
-    double dy = planet.getY() = node.xy.y;
-    double distance = sqrt(dx*dx +dy*dy);
-    
-
-
-    // if distance bettween
-
-    return ;
+NetForce netForce(Planet planet, Square *node)
+{
+    if (!node)
+        return NetForce(0, 0); // node null, no children aka no gravity
+    long double distance = calculate_distance(planet.getXY(), node->xy);
+    long double gravity;
+    NetForce ret = NetForce(0, 0);
+    bool isSufficientlyFar = distance > planet.getSquare()->size / 4; // tetragwno ara pleura size/4
+    bool hasPlanet = node_contains_planet(planet, node);
+    if (isSufficientlyFar && !hasPlanet)
+    {
+        // calculate the gravity of all bodies in the node
+        // center of mass being the node's center of mass
+        gravity = calculate_gravity(planet.getMass(), node->mass, distance);
+        ret.fx = (gravity * (node->xy.x - planet.getX())) / distance;
+        ret.fy = (gravity * (node->xy.y - planet.getY())) / distance;
+    }
+    else if (!isSufficientlyFar || hasPlanet)
+    {
+        // recursivly calculate gravity the children exert at the body
+        ret += netForce(planet, node->ne);
+        ret += netForce(planet, node->nw);
+        ret += netForce(planet, node->se);
+        ret += netForce(planet, node->sw);
+        // sunhstamenh twn  dynamewn einai to a8roisma twn dunamewn
+    }
+    return ret;
 }
-
-
-
-void computeForces(){
-    return;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-void barnes_hut(){
-    
-}
-
-
 
 // ********************** MAIN ************************
-int main(int argc, char* argv[]) {
-    if(argc <2){
-        std::cerr<<"Usage: ./a.out"<<argv[0]<< "<filename> <iterations_num> <thread_num>"<<std::endl;
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        std::cerr << "Usage: ./a.out" << argv[0] << "<filename> <iterations_num> <thread_num>" << std::endl;
         return 1;
     }
     std::ifstream file(argv[1]);
-    if(!file){
-        std::cerr << "Failed to open file "<<argv[1] << std::endl;
+    if (!file)
+    {
+        std::cerr << "Failed to open file " << argv[1] << std::endl;
         return 1;
     }
 
-    long long iterations = argv[2]; // 'seconds' the simmulation will run
-    int thread_num = argv[3];
+    long long iterations = std::stoll(argv[2]); // 'seconds' the simmulation will run
+    int thread_num = std::stoll(argv[3]);
     long double numPlanets;
     long double universeSize;
     long double i;
@@ -92,31 +83,66 @@ int main(int argc, char* argv[]) {
     std::string name;
 
     file >> numPlanets >> universeSize;
-    std::cout<< "Number of planets: " << numPlanets << std::endl;
-    std::cout<< "Universe size: " << universeSize << std::endl;
+    std::cout << "Number of planets: " << numPlanets << std::endl;
+    std::cout << "Universe size: " << universeSize << std::endl;
     std::vector<Planet> planets;
-    for(int i=0; i<numPlanets; i++){
-        file >> x >> y>> velocityX >> velocityY >> mass >> name;
-        Planet planet(x,y,velocityX,velocityY,mass,name);
+    for (int i = 0; i < numPlanets; i++)
+    {
+        file >> x >> y >> velocityX >> velocityY >> mass >> name;
+        Planet planet(CoordinatesXY(x, y), velocityX, velocityY, mass, name);
         planets.push_back(planet);
     }
-    for (auto& planet : planets) {
+    for (auto &planet : planets)
+    {
         std::cout << "Name: " << planet.getName() << ", "
                   << "Position: (" << planet.getX() << ", " << planet.getY() << "), "
                   << "Velocity: (" << planet.getVelocityX() << ", " << planet.getVelocityY() << "), "
                   << "Mass: " << planet.getMass() << std::endl;
     }
-
+    // return 0;
     // its gonna create the universe along with the bhtree
     // maybe change the name to universe again cause i dont like the square
-    
-    for( i=0; i<iterations; i++){
-        std::cout<<"Creating tree: " << i << std::endl;
-        Square universe = Square(&planets,CoordinatesXY(0,0)); // hide all the complexity
+    for (i = 0; i < iterations; i++)
+    {
+        std::cout << "Creating tree: " << i << std::endl;
+        Square universe = Square(&planets, CoordinatesXY(0, 0), universeSize); // hide all the complexity
+
+        parallel_for(
+            tbb::blocked_range<int>(0, planets.size()),
+            [&](const tbb::blocked_range<int> &range) -> void
+            {
+                int j;
+                NetForce force;
+                for (j = range.begin(); j != range.end(); j++)
+                {
+                    force = netForce(planets[j], &universe);
+                    planets[j].setVelocityX(force.fx / planets[j].getMass());
+                    planets[j].setVelocityY(force.fy / planets[j].getMass());
+                }
+            });
+
+
+        parallel_for(
+            tbb::blocked_range<int>(0, planets.size()),
+            [&](const tbb::blocked_range<int> &range) -> void
+            {
+                int j;
+                for (j = range.begin(); j != range.end(); j++)
+                {
+                    planets[j].gotoNextPosition();
+                }
+            });
+        
+        std::cout << "Iteration: " << i << std::endl;
+        for (auto &planet : planets)
+        {
+            std::cout << "Name: " << planet.getName() << ", "
+                    << "Position: (" << planet.getX() << ", " << planet.getY() << "), "
+                    << "Velocity: (" << planet.getVelocityX() << ", " << planet.getVelocityY() << "), "
+                    << "Mass: " << planet.getMass() << std::endl;
+        }
         
     }
 
-    
     return 0;
 }
-
--- ask3.h
+++ ask3.h
@@ -5,32 +5,47 @@
 #include <string>
 #include <vector>
 #include <time.h>
-#include "tbb/tbb.h"
 #include <math.h>
-
-
-
+#include <tbb/parallel_for.h>
 
 struct timespec start;
 double elapsed;
-const long double G=6.673e-11;
-const  double theta=0.5;
+const double theta = 0.5;
 int N;
 
-class CoordinatesXY{
-    public:
-        long double x;
-        long double y;
-    CoordinatesXY(long double x, long double y):x(x),y(y){}
-    CoordinatesXY():x(0),y(0){}
+class CoordinatesXY
+{
+public:
+    long double x;
+    long double y;
+    CoordinatesXY(long double x, long double y) : x(x), y(y) {}
+    CoordinatesXY() : x(0), y(0) {}
 
-    bool operator==(const CoordinatesXY& other){
+    bool operator==(const CoordinatesXY &other)
+    {
         return (x == other.x) && (y == other.y);
     }
 };
 class Square;
+class Planet;
 
-class Planet {
+class NetForce
+{
+public:
+    long double fx;
+    long double fy;
+    NetForce(long double fx, long double fy) : fx(fx), fy(fy) {}
+    NetForce() : fx(0), fy(0){};
+    NetForce &operator+=(const NetForce &other)
+    {
+        fx += other.fx;
+        fy += other.fy;
+        return *this;
+    }
+};
+
+class Planet
+{
 private:
     CoordinatesXY xy;
     long double velocityX;
@@ -38,14 +53,21 @@ private:
     long double mass;
     int exploded;
     std::string name;
-    Square& square; // square the leaf it belongs
+    Square *square = nullptr; // square the leaf it belongs
 public:
+    
     // Constructor
-    Planet(long double x, long double y, long double velocityX, long double velocityY, long double mass, const std::string& name) 
-        : x(x), y(y), velocityX(velocityX), velocityY(velocityY), mass(mass), name(name) {}
+    Planet(CoordinatesXY xy, long double velocityX, long double velocityY, long double mass, const std::string &name)
+        : xy(xy), velocityX(velocityX), velocityY(velocityY), mass(mass), name(name) {}
 
     // Default constructor
-    Planet() : x(0), y(0), velocityX(0), velocityY(0), mass(0), name("") {}
+    Planet() : xy(CoordinatesXY(0,0)), velocityX(0), velocityY(0), mass(0), name("") {}
+
+    // operator overloads
+    bool operator==(const Planet &other)
+    {
+        return xy == other.xy && velocityX == other.velocityX && velocityY == other.velocityY && name == other.name;
+    }
 
     // Getter functions
     CoordinatesXY getXY() { return xy; }
@@ -56,143 +78,161 @@ public:
     long double getMass() { return mass; }
     int hasExploed() { return exploded; }
     std::string getName() { return name; }
-    Square& getSquare() { return square;}
+    Square *getSquare() { return square; }
 
     // Setter functions
     void setXY(CoordinatesXY xy) { this->xy = xy; }
     void setVelocityX(long double velocityX) { this->velocityX = velocityX; }
     void setVelocityY(long double velocityY) { this->velocityY = velocityY; }
     void setMass(long double mass) { this->mass = mass; }
-    void setName(const std::string& name) { this->name = name; } // wonder if this can cause a bug
-    // void setSquare(Square *square) { this->square = square; }
+    void setName(const std::string &name) { this->name = name; } // wonder if this can cause a bug
+    void setSquare(Square *square) { this->square = square; }
     // Other
-    void initiateExplostion(){
+    void initiateExplostion()
+    {
         // extreme case
-        std::cout << "Planet with coordinates ("<< CoordinatesXY.x << " , " << CoordinatesXY.y <<") has exploded." << std::endl;
+        std::cout << "Planet with coordinates (" << xy.x << " , " << xy.y << ") has exploded." << std::endl;
         exploded = 1;
         return;
     }
-    
 
-
+    void gotoNextPosition()
+    {
+        const int dt = 1;
+        long double vx = xy.x + dt * velocityX;
+        long double vy = xy.y + dt * velocityY;
+        xy = CoordinatesXY(xy.x + dt * vx, xy.y + dt * vy);
+        return;
+    }
 };
 
 class Square // universe
 {
 public:
+    long double size;
     long long int id;
     CoordinatesXY xy;
-    long double  size; //TODO add it
     CoordinatesXY centerMass;
     long double mass;
-    std::vector<Planet>& planets;
+    std::vector<Planet> *planets;
     std::vector<long long int> planetIndexes; // ideally pointer
     long long level;
-    Square *nw, *ne, *sw, *se,*parent;
+    Square *nw, *ne, *sw, *se, *parent;
 
-    CoordinatesXY calculate_centerMass(Square *sq1, Square *sq2){
-        long double x,y;
+    CoordinatesXY calculate_centerMass(Square *sq1, Square *sq2)
+    {
+        long double x, y;
         long double mass = sq1->mass + sq2->mass;
-        x=(sq1->centerMass.x*sq1->mass + sq2->centerMass.x*sq2->mass)/mass;
-        x=(sq1->centerMass.y*sq1->mass + sq2->centerMass.y*sq2->mass)/mass;
-        return CoordinatesXY(x,y);
+        x = (sq1->centerMass.x * sq1->mass + sq2->centerMass.x * sq2->mass) / mass;
+        x = (sq1->centerMass.y * sq1->mass + sq2->centerMass.y * sq2->mass) / mass;
+        return CoordinatesXY(x, y);
     }
-    
-    void buildTree(){
-        this->mass=0;
+
+    void buildTree()
+    {
+        this->mass = 0;
         centerMass = xy;
-        
-        if(planetIndexes.size() <2){
+
+        if (planetIndexes.size() < 2)
+        {
             // assign the planet the square it belongs, aka the leaf
-            if(planetIndexes.size() == 1){
-                planets[planetIndexes[0]].setSquare(this);
-                mass=(planets)[planetIndexes[0]];
+            if (planetIndexes.size() == 1)
+            {
+                (*planets)[planetIndexes[0]].setSquare(this);
+                mass = (*planets)[planetIndexes[0]].getMass();
             }
             return;
         }
         std::vector<long long int> nwPlanets, nePlanets, swPlanets, sePlanets;
-        for (size_t i = 0; i < planetIndexes.size(); i++) {
-            const Planet planet = planets[i]; 
-
-            if (planet.getXY() == xy) {
-                planet.initiateExplostion();
+        for (size_t i = 0; i < planetIndexes.size(); i++)
+        {
+            if ((*planets)[i].getXY() == xy)
+            {
+                (*planets)[i].initiateExplostion();
                 continue;
             }
-            if(planet.hasExploed())continue;
+            if ((*planets)[i].hasExploed())
+                continue;
 
-            if (planet.getX() < xy.x && planet.getY() > xy.y) {
-                nwPlanets.push_back(i); 
-            } else if (planet.getX() > xy.x && planet.getY() > xy.y) {
-                nePlanets.push_back(i); 
-            } else if (planet.getX() < xy.x && planet.getY() < xy.y) {
-                swPlanets.push_back(i); 
-            } else {
-                sePlanets.push_back(i); 
+            if ((*planets)[i].getX() < xy.x && (*planets)[i].getY() > xy.y)
+            {
+                nwPlanets.push_back(i);
+            }
+            else if ((*planets)[i].getX() > xy.x && (*planets)[i].getY() > xy.y)
+            {
+                nePlanets.push_back(i);
+            }
+            else if ((*planets)[i].getX() < xy.x && (*planets)[i].getY() < xy.y)
+            {
+                swPlanets.push_back(i);
+            }
+            else
+            {
+                sePlanets.push_back(i);
             }
 
-            nw = new Square(planets,CoordinatesXY(-xy.x/2 , xy.y/2) ,nwPlanets   ,level+1 ,this);
-            mass +=nw->mass;
-            centerMass = this.calculate_centerMass(this,nw); // 4 fores giati dinetai o typos mono gia 2 swmata sthn ekfwnhsh....
+            nw = new Square(planets, CoordinatesXY(-xy.x / 2, xy.y / 2), size / 4, nwPlanets, level + 1, this);
+            mass += nw->mass;
+            centerMass = this->calculate_centerMass(this, nw); // 4 fores giati dinetai o typos mono gia 2 swmata sthn ekfwnhsh....
 
-            ne = new Square(planets,CoordinatesXY(xy.x/2 , xy.y/2)  ,nePlanets   ,level+1 ,this);
-            mass +=ne->mass;
-            centerMass = this.calculate_centerMass(this,ne);
+            ne = new Square(planets, CoordinatesXY(xy.x / 2, xy.y / 2), size / 4, nePlanets, level + 1, this);
+            mass += ne->mass;
+            centerMass = this->calculate_centerMass(this, ne);
 
-            sw = new Square(planets,CoordinatesXY(-xy.x/2 , -xy.y/2),swPlanets   ,level+1 ,this);
-            mass +=sw->mass;
-            centerMass = this.calculate_centerMass(this,sw);
+            sw = new Square(planets, CoordinatesXY(-xy.x / 2, -xy.y / 2), size / 4, swPlanets, level + 1, this);
+            mass += sw->mass;
+            centerMass = this->calculate_centerMass(this, sw);
 
-            se = new Square(planets,CoordinatesXY(xy.x/2 , -xy.y/2) ,sePlanets   ,level+1 ,this);
-            mass +=se->mass;
-            centerMass = this.calculate_centerMass(this,se);
-
+            se = new Square(planets, CoordinatesXY(xy.x / 2, -xy.y / 2), size / 4, sePlanets, level + 1, this);
+            mass += se->mass;
+            centerMass = this->calculate_centerMass(this, se);
         }
     }
-    
-    Square( std::vector<Planet>& planets,CoordinatesXY xy):planets(planets),xy(xy),level(0),parent(NULL){
+
+    Square(std::vector<Planet> *planets, CoordinatesXY xy, long double size) : planets(planets), xy(xy), size(size), level(0), parent(NULL)
+    {
         int i;
-        for (i = 0; i < planets->size(); i++){
+        for (i = 0; i < planets->size(); i++)
+        {
             planetIndexes.push_back(i);
         }
         buildTree();
     }
-    Square( std::vector<Planet>& planets,CoordinatesXY xy,std::vector<long long int> planetIndexes,long long int level,Square *parent)
-        :planets(planets),xy(xy),planetIndexes(planetIndexes),level(level),parent(parent){
+    Square(std::vector<Planet> *planets, CoordinatesXY xy, long double size, std::vector<long long int> planetIndexes, long long int level, Square *parent)
+        : planets(planets), xy(xy), size(size), planetIndexes(planetIndexes), level(level), parent(parent)
+    {
+        this->id = 0;
         buildTree();
     }
-
 };
 
-class Universe{
+class Universe
+{
     std::vector<Planet> planets;
     Square *root;
+    long long int size;
 
-    Universe(std::vector<Planet> planets):planets(planets){
-        root = new Square(&planets,CoordinatesXY(0,0));
+    Universe(std::vector<Planet> planets, long long int size) : planets(planets), size(size)
+    {
+        root = new Square(&planets, CoordinatesXY(0, 0), size);
     }
-}
+};
 
-long double netForce(Planet planet, BHTree tree){
-        return 1;
-}
-
-
+NetForce netForce(Planet planet, Square *node);
 
 /**
  * New position of the planet? Man...
-*/
+ */
 CoordinatesXY newPosition(Planet p, CoordinatesXY xy);
-
 
 // must be recursive, divide into 4 pieces and each piece divide
 // into 4, untill there are 0 or 1 bodies left
 void barnes_hut();
 
-
 /**
  * Calculate the gravity
-*/
-long double calculate_gravity(Planet p1, Planet p2);
+ */
+long double calculate_gravity(long double m1, long double m2, long double distance);
 
 void parseInput(char **input);
 
